bbbdd58c4d2479b6c05e04aadef2c886bcaee872
Alex <correo>
2025-02-19 20:33:16 +0100

    Cargando mas ejercicios

---
+++ clases/Textos.py
@@ -0,0 +1,10 @@
+print("Texto 1")
+print("\n")
+print('Texto 2')
+
+# Comentario de unas sola linea
+
+'''
+Comentario 1
+Comentario 2
+'''
---
+++ clases/Variables.py
@@ -0,0 +1,26 @@
+nombre = "Alex"
+print("NOMBRE: "+ nombre)
+
+len(nombre)
+
+edad = 99
+print("EDAD: "+ str(edad))
+
+isConductor = True
+isAnciano = False
+
+print("ES CONDUCTOR: "+ str(isConductor))
+
+datoNulo = None
+
+
+numero1 = 4
+numero2 = 3
+
+print("SUMA: "+ str(numero1 + numero2))
+print(numero1-numero2)
+print(numero1 / numero2)#Div. normal
+print(numero1 // numero2)#Div. entera
+print(numero1 * numero2)
+print(numero1 ** numero2)#Exponente
+print(numero1 ** 0.5)#Raiz cuadrada
